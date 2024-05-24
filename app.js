f4082a16385a2bdfedf36bddacac336389fc22aa
D-I-V-Y-A-S <divyaselvam1405@gmail.com>
2024-05-24 09:17:16 +0530

    Final Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()            //dev dependency 
+const mongoose=require('mongoose')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3500
+
+const cors = require('cors')                             
+const jobRouter = require('./Router/jobRouter')
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.DB_URL)
+const db = mongoose.connection
+db.on('error', (errorMessage) => console.log(errorMessage))
+db.once('open', () => console.log('Connected to db successfully'))
+
+app.use('/api/v1/job', jobRouter)
+app.use(express.urlencoded({extended:true}))
+app.listen(PORT,console.log(`Server started running on http://localhost:${PORT}/api/v1/job`))
